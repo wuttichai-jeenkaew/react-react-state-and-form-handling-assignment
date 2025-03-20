260b5d8b8c03059c80a2c1f281d6c7c75827cb36
wuttichai-jeenkaew <wuttichai.jeenkaew@gmail.com>
2025-03-20 16:11:13 +0700

    Assignment-state

--- src/components/ProductForm.jsx
+++ src/components/ProductForm.jsx
@@ -1,6 +1,75 @@
+import { useState } from "react";
+
+
+
 function ProductForm() {
+  const [name, setName] = useState("");
+  const [image, setImage] = useState("");
+  const [price, setPrice] = useState("");
+  const [description, setDescription] = useState("");
+  const [email, setEmail] = useState("");
+  const [errors, setErrors] = useState({});
+
+  function isValidEmail(email) {
+    return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+  }
+
+  function validateForm() {
+    let newErrors = {};
+    if (!name) {
+      newErrors.name = "Name is required.";
+    }
+    if (!image) {
+      newErrors.image = "Image URL is required.";
+    }
+    if (!price) {
+      newErrors.price = "Price is required.";
+    } else if (price && price < 0) {
+      newErrors.price = "Price cannot be less than 0.";
+    }
+    if (!description) {
+      newErrors.description = "Description is required.";
+    }
+    if (!email) {
+      {
+        newErrors.email = "Email is required.";
+      }
+    } else if (!isValidEmail(email)) {
+      newErrors.email = "Invalid email format.";
+    }
+    console.log(Object.keys(newErrors).length);
+    setErrors(newErrors);
+    return Object.keys(newErrors).length === 0;
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    if (!validateForm()) {
+      return;
+    }
+
+    let newFormData = {
+      name: name,
+      image: image,
+      price: price,
+      description: description,
+      email: email,
+    };
+
+    alert(JSON.stringify(newFormData));
+
+    setName("");
+    setImage("");
+    setPrice("");
+    setDescription("");
+    setEmail("");
+    setErrors({});
+  }
+  
+
+
   return (
-    <form className="post-form">
+    <form className="post-form" onSubmit={handleSubmit}>
       <h1>Create Product Form</h1>
       <div className="input-container">
         <label>
@@ -10,9 +79,11 @@ function ProductForm() {
             name="name"
             type="text"
             placeholder="Enter name here"
-            onChange={() => {}}
+            value={name}
+            onChange={(event) => setName(event.target.value)}
           />
         </label>
+        {errors.name && <p className="error-message">{errors.name}</p>}
       </div>
       <div className="input-container">
         <label>
@@ -22,9 +93,11 @@ function ProductForm() {
             name="image"
             type="text"
             placeholder="Enter image url here"
-            onChange={() => {}}
+            value={image}
+            onChange={(event) => setImage(event.target.value)}
           />
         </label>
+        {errors.image && <p className="error-message">{errors.image}</p>}
       </div>
       <div className="input-container">
         <label>
@@ -34,9 +107,11 @@ function ProductForm() {
             name="price"
             type="number"
             placeholder="Enter price here"
-            onChange={() => {}}
+            value={price}
+            onChange={(event) => setPrice(event.target.value)}
           />
         </label>
+        {errors.price && <p className="error-message">{errors.price}</p>}
       </div>
       <div className="input-container">
         <label>
@@ -46,11 +121,13 @@ function ProductForm() {
             name="description"
             type="text"
             placeholder="Enter description here"
-            onChange={() => {}}
+            value={description}
+            onChange={(event) => setDescription(event.target.value)}
             rows={4}
             cols={30}
           />
         </label>
+        {errors.description && <p className="error-message">{errors.description}</p>}
       </div>
       <div className="input-container">
         <label>
@@ -60,9 +137,11 @@ function ProductForm() {
             name="email"
             type="email"
             placeholder="Enter your email here"
-            onChange={() => {}}
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
           />
         </label>
+        {errors.email && <p className="error-message">{errors.email}</p>}
       </div>
       <div className="form-actions">
         <button type="submit">Create</button>
